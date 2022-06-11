feb0ad113517e2dd2ee5fa64e2b62f48aa9970e6
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-06-11 20:48:02 -0300

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,1 +1,13 @@
+$psheesh = Invoke-WebRequest https://raw.githubusercontent.com/bredsan/psheesh/psheesh.ps1 | Invoke-Expression
+$dev = Invoke-WebRequest https://raw.githubusercontent.com/bredsan/psheesh/dev.ps1 | Invoke-Expression
 
+
+function psheesh {
+    # Carrega o perfil ESTI
+    $psheesh
+}
+
+function dev {
+    # Carrega o perfil DEV
+    $dev
+}
